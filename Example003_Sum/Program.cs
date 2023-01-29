c8d3d803b633e68c188032ce0d64db7c68f4870f
Fufaev <georijf@bk.ru>
2023-01-29 23:36:12 +0500

    add Random in Example 003 Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine($"Первое рандомное число в диапозоне от 1 до 10 это: {numberA}");
+int numberB = new Random().Next(1, 10);
+Console.WriteLine($"Второе рандомное число в диапозоне от 1 до 10 это: {numberB}");
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine($"Сумма рандомных чисел: {result}");
