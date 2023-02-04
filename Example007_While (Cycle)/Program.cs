caf0156eed23921e2ef9e42889462157a0ba38aa
Fufaev <georijf@bk.ru>
2023-02-04 20:50:21 +0500

    Added files and modified ex 001 007

--- Example007_While (Cycle)/Program.cs
+++ Example007_While (Cycle)/Program.cs
@@ -15,27 +15,28 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
+
 int x = xa, y = ya;
 
 int count = 0;
 
 while (count < 10000)
 {
-    int what = new Random().Next(0, 3); // 0, 1, 2
+    int what = new Random().Next(0, 3);
     if (what == 0)
     {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
+        x = (x + xa) / 2; // x = (40 + 40) / 2 = 40
+        y = (y + ya) / 2; // y = (1 + 1) / 2 = 1
     }
     if (what == 1)
     {
-        x = (x + xb) / 2;
-        y = (y + yb) / 2;
+        x = (x + xb) / 2; // x = (40 + 1) / 2 = 20
+        y = (y + yb) / 2; // y = (1 + 30) /2 = 15
     }
     if (what == 2)
     {
-        x = (x + xc) / 2;
-        y = (y + yc) / 2;
+        x = (x + xc) / 2; // x = (40 + 80) / 2 = 60
+        y = (y + yc) / 2; // y = (1 + 30) /2 = 15
     }
 
     Console.SetCursorPosition(x, y);
