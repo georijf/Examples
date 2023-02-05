06bafee3cf48299640d20c2cbd0e0eb2cbc9a258
Fufaev <georijf@bk.ru>
2023-02-05 12:39:25 +0500

    Added Example 010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿//               0   1   2   3   4   5   6   7   8 
+int[] array = { 31, 52, 47, 23, 47, 55, 66, 74, 68 };
+
+int n = array.Length;
+int find = 47;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // при наличии одинаковых елементов в массиве, этот оператор заканчивает работу цикла на 1м найденном значении.
+    }
+    index++;
+}
+
